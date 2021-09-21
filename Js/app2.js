bf539cc7e0d25eccc14434e8c58f7138cad96e3a
saamlegend <oyerooluwatosin@gmail.com>
2021-09-21 22:33:10 +0300

    Variable lesson

---
+++ Js/app2.js
@@ -0,0 +1,10 @@
+// Various varibale declaration with Java
+
+//We can use the following var, let, const
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+//Note: you can always reassign values when you use Var and let
