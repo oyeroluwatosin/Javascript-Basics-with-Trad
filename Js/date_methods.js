aade0c5458784cf58c59cb371a672cb3542cfccf
oyerooluwatosin <42722536+oyeroluwatosin@users.noreply.github.com>
2021-09-24 00:41:54 +0300

    date_method

---
+++ Js/date_methods.js
@@ -0,0 +1,18 @@
+let val;
+
+const today = new Date();
+
+// val = today;
+val = today.getMonth();
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+val = today.getTime();
+
+console.log(val);
+console.log(typeof val);
