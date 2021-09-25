c978cf5950a42dc70579da4135d13868453e3bb2
oyerooluwatosin <42722536+oyeroluwatosin@users.noreply.github.com>
2021-09-25 12:12:35 +0300

    window_object

---
+++ Js/window_object.js
@@ -0,0 +1,54 @@
+// Window methods / objects / properties
+
+// Alert
+// alert('Hello World');
+
+//Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if(confirm('Are you sure')){
+//   console.log('YES');
+// } else{
+//   console.log('No');
+// }
+
+let val;
+
+// Outer height and width
+// val = window.outerHeight;
+// val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+//location object
+val =window.location;
+val =window.location.hostname;
+val =window.location.port;
+val =window.location.href;
+val =window.location.search;
+
+// Redirect
+//window.location.href ='http://google.com';
+
+// Reload
+//window.location.reload();
+
+// History Object
+
+window.history.go(-1);
+val = window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appVersion;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+console.log(val);
